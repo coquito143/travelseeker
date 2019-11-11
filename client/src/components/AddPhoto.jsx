bf431f80948a4f9f86086546bc8534eec177283d
Ian Rappaport <ianrappaport@Ians-MacBook-Pro-2.local>
2019-11-11 10:16:47 -0500

    got add photo component on screen

---
+++ client/src/components/AddPhoto.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+export default class AddPhoto extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+
+  }
+}
+
