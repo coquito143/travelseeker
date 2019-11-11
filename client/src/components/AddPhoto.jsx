a4c691ff8fac628415cf2c1a2abdbe37c92d5d33
Ian Rappaport <ianrappaport@Ians-MacBook-Pro-2.local>
2019-11-11 10:44:46 -0500

    added inputs for picture description and image url for add photo

--- client/src/components/AddPhoto.jsx
+++ client/src/components/AddPhoto.jsx
@@ -4,13 +4,53 @@ import React, { Component } from 'react';
 
 export default class AddPhoto extends Component {
   state = {
+    image_url: "",
+    description: ""
+  }
 
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    })
   }
 
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <div>
+          Blank Space
+        </div>
+        <div>
+          Blank Space
+        </div>
+        <div>
+          Blank Space
+        </div>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+
+        }}>
+          <label htmlFor="image_url">Image URL</label>
+          <input
+            type="text"
+            name="image_url"
+            id="image_url"
+            value={this.state.image_url}
+            onChange={this.handleChange}
+          />
+          <br />
+          <label htmlFor="description">picture description</label>
+          <input
+            type="text"
+            name="description"
+            id="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+          <br />
+<button>Submit Photo</button>
+        </form>
       </div>
     )
 
