d61dc98cc17f56553d651672657bcf93d15858b0
Ian Rappaport <ianrappaport@Ians-MacBook-Pro-2.local>
2019-11-12 12:47:42 -0500

    updated version

--- client/src/components/AddPhoto.jsx
+++ client/src/components/AddPhoto.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { addPhoto } from '../services/api-helper';
 
 
 
 export default class AddPhoto extends Component {
   debugger;
   state = {
-
+    userId: null,
+    countryId: null,
     image_url: "",
     description: ""
   }
@@ -17,14 +20,30 @@ export default class AddPhoto extends Component {
     })
   }
 
+  handleSubmit = async (e) => {
+    e.preventDefault()
+    debugger;
+    const response = await addPhoto(this.state);
+    const photo = response.data;
+    // this.setState(prevState => ({
+    //   items: [...prevState.characters, character]
+    // }))
+    // this.props.history.push('/')
+  }
+
+  componentDidMount = (props) => {
+    const userId = this.props.userId;
+    const countryId = this.props.countryId;
+    this.setState({
+      userId, countryId
+    })
+  }
+
   render() {
     return (
 
       <div class="addPhoto">
-        <form class="addPhotoForm" onSubmit={(e) => {
-          e.preventDefault();
-
-        }}>
+        <form class="addPhotoForm" onSubmit={this.handleSubmit} >
           {/* <select>
             {
               this.props.countries.map((country) => (
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use('/countries', countryRouter);
 app.use('/users', userRouter);
-app.use('/users/:userId/photos/:photoId', photoRouter);
+app.use('/photos', photoRouter);
 
 
 
