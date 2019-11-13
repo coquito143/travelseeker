6456524e4de2844ee28b5b58676b00f70bb95a01
Melanie Phan <melanie.phan@prudential.com>
2019-11-13 16:01:15 -0500

    styled homepage

--- client/src/components/CountriesList.jsx
+++ client/src/components/CountriesList.jsx
@@ -7,13 +7,19 @@ export default function CountriesList(props) {
     <div id="countries-div">
       <div id="view-countries-text1"><h2>Countries</h2></div>
                   <div id="view-countries-text2">
-                    <span><h2>where the dollar goes farthest</h2></span>
-                  </div>
+        <span><h2>where the dollar goes farthest</h2></span>
+   
+      </div>
+      <div className="countries-list-div">
         {props.countries.map(country => (
         <React.Fragment key={country.id}>
-          <Link to={`/countries/${country.id}`}><h3>{country.country_name}</h3></Link>
+            <Link to={`/countries/${country.id}`}>
+              <img src={country.image_url}/>
+              <h3>{country.country_name}</h3>
+            </Link>
         </React.Fragment>
-      ))}
+        ))}
+        </div>
       {/* <Link to="/country/new"><button>Add an post</button></Link> */}
     </div>
   )
